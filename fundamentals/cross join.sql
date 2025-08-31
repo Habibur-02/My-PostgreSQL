c6a67328686887a5d07a37d020b3c2a63fb25ed8
Md. Habibur Rahman <156560375+Habibur-02@users.noreply.github.com>
2025-08-31 13:14:10 +0600

    Create cross join.sql

---
+++ fundamentals/cross join.sql
@@ -0,0 +1,32 @@
+-- Students table তৈরি
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+-- Students table এ data insert
+INSERT INTO students (name) VALUES
+('Rahim'),
+('Karim'),
+('Anika');
+
+CREATE TABLE course (
+    id SERIAL PRIMARY KEY,
+    course_name VARCHAR(100) NOT NULL,
+    level VARCHAR(20) NOT NULL
+);
+
+INSERT INTO course (course_name, level) VALUES
+('Math', 'Basic'),
+('Physics', 'Basic'),
+('AI', 'Advance'),
+('DBMS', 'Advance');
+
+select * from course;
+select * from students;
+
+select  * 
+from course as c
+cross join students
+where (length(students.name)>=5 and c.level='Basic')
+;
