e342fda512efa0beeed65738875c34adea14c292
Md. Habibur Rahman <156560375+Habibur-02@users.noreply.github.com>
2025-09-10 21:59:35 +0600

    Add average processing time query for machines
    Add query to calculate average processing time per machine.

--- fundamentals/join.sql
+++ fundamentals/join.sql
@@ -429,6 +429,11 @@ from Weather as w
 Right join Employees as e
 on w.id=e.emp_id;
 
+select a.machine_id, ROUND(AVG(b.timestamp - a.timestamp)::numeric, 3) as processing_time
+from Activity as a
+join Activity as b
+on a.machine_id=b.machine_id and a.process_id=b.process_id and a.activity_type='start' and b.activity_type='end'
+group by a.machine_id;
 
 
 
